2589d594e445c18dc49d1d4e578ad8193ed18648
Mohamed Shimy <msh.work@outlook.sa>
2023-05-20 20:01:25 +0300

    Create inRange methods for Comparable objects.

---
+++ Sources/SHKit/Shared/Extensions/Foundation/Comparable.swift
@@ -0,0 +1,25 @@
+//
+//  Comparable.swift
+//  
+//
+//  Created by Mohamed Shemy on 20/05/2023.
+//
+
+import Foundation
+
+public extension Comparable {
+    
+    func inRange(closed lower: Self, _ upper: Self) -> Bool {
+        guard lower <= upper else { return false }
+        return self >= lower && self <= upper
+    }
+    
+    func inRange(opened lower: Self, _ upper: Self) -> Bool {
+        guard lower < upper else { return false }
+        return self >= lower && self < upper
+    }
+    
+    func inRange<C>(of collection: C) -> Bool where C: Collection, C.Index == Self {
+        self.inRange(opened: collection.startIndex, collection.endIndex)
+    }
+}
---
+++ Sources/SHKit/SwiftUI/Components/SwipeGesture.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Mohamed Shemy on 20/05/2023.
+//
+
+import Foundation
