00a3b7a04c7d796f384bfa12178e40137218a091
Mohamed Shimy <msh.work@outlook.sa>
2023-05-26 19:01:50 +0300

    Data Extensions
     - Map to prettyPrintedJSONString.
     - Map to dictionary.

---
+++ Sources/SHKit/Foundation/Extensions/Data.swift
@@ -0,0 +1,24 @@
+//
+//  Data.swift
+//  
+//
+//  Created by Mohamed Shemy on 26/05/2023.
+//
+
+import Foundation
+
+public extension Data {
+    
+    var prettyPrintedJSONString: NSString? {
+        guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
+              let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
+              let prettyPrintedString = NSString(data: data, encoding: String.Encoding.utf8.rawValue) else { return nil }
+        
+        return prettyPrintedString
+    }
+    
+    var toDictionary: [String: Any] {
+        return (try? JSONSerialization.jsonObject(with: self, options: .allowFragments))
+            .flatMap { $0 as? [String: Any] } ?? [:]
+    }
+}
