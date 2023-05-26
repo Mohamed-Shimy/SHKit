5581d47925e7aa749042ea17fc2897447384f737
Mohamed Shimy <msh.work@outlook.sa>
2023-05-26 19:54:21 +0300

    Downgrade Package version to iOS/tvOS 13 and macOS 10.15

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SHKit",
     platforms: [
-        .iOS(.v14), .macOS(.v11), .tvOS(.v14)
+        .iOS(.v13), .macOS(.v10_15), .tvOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
